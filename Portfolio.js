1130b9c3490ab24e402eef2a93ba347c1385311f
afeefa <afeehahamza@gmail.com>
2024-06-13 22:57:59 +0530

    first commit

---
+++ Portfolio.js
@@ -0,0 +1,24 @@
+// function sendMail(){
+//     var params ={
+//         name: document.getElementById("name").value ,
+//         email: document.getElementById("email").value ,
+//         massage: document.getElementById("massage").value ,
+//     };
+
+
+
+// const serviceID ="service_uooo5o5";
+// const templateID="template_4u2pqyi";
+
+// emailjs.send(serviceID,templateID,params)
+// .then(
+//     res => {
+//         document.getElementById("name").value = "";
+//         document.getElementById("email").value = "";
+//         document.getElementById("massage").value = "";
+//         console.log(res);
+//         alert("your massage sent succesfully");
+//     }
+// )
+// .catch((err) => console.log(err));
+// }
